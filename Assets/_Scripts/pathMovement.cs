b311f300aafb9ea71e768a5660db2a0061bc0df3
Nicholas Randall <30700271+cidetraq@users.noreply.github.com>
2021-03-08 07:14:49 -0600

    improved path movement script

---
+++ Assets/_Scripts/pathMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pathMovement : MonoBehaviour
+{
+    public GameObject pathExplorerCharacter;
+    public float moveSpeed;
+    float timer;
+    static Vector3 currentPositionHolder;
+    int currentNode;
+    static Vector3 startPositionEachUpdate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CheckNode();
+    }
+
+    void CheckNode(){
+        startPositionEachUpdate = pathExplorerCharacter.transform.position;
+        timer = 0;
+        currentPositionHolder = gameObject.transform.GetChild(currentNode).transform.position;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime * moveSpeed;
+        if (pathExplorerCharacter.transform.position != currentPositionHolder)
+        {
+            pathExplorerCharacter.transform.position = Vector3.MoveTowards(startPositionEachUpdate, currentPositionHolder, timer);
+        }
+        else
+        {
+            if (currentNode < gameObject.transform.childCount - 1)
+            {
+                currentNode++;
+                CheckNode();
+            }
+            else
+            {
+                currentNode = 0;
+                print(currentNode);
+                CheckNode();
+            }
+        }
+    }
+}
